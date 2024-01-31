6869dd3c8302a8f679045d772e9aa14c6df99954
MiriamEvardi <miriamevardi97@gmail.com>
2024-01-31 16:35:03 +0100

    removed unused classes on js, exercise completed

--- js/script.js
+++ js/script.js
@@ -19,14 +19,12 @@ for (let i = 1; i <= 100; i++) {
 
     // (IF) voglio che i multipli di 3 abbiano stampato "Fizz" al posto del numero
     if (i % 3 == 0) {
-        squareElement.className += " fizz";
         squareElement.innerText = "Fizz";
         squareElement.style.backgroundColor = "rgb(182, 100, 241)";
     }
 
     // (IF) voglio che i multipli di 5 abbiano stampato "Buzz"
     if (i % 5 == 0) {
-        squareElement.className += " buzz";
         squareElement.innerText = "Buzz";
         squareElement.style.backgroundColor = "rgb(81, 157, 255)";
     }
@@ -34,7 +32,6 @@ for (let i = 1; i <= 100; i++) {
     // (IF) voglio che sia i multipli di 3 che di 5 abbiano stampato "FizzBuzz"
     // altrimenti fare la moltiplicazione 3 * 5 ?
     if (i % 3 == 0 && i % 5 == 0) {
-        squareElement.className = "square fizz buzz";
         squareElement.innerText = "FizzBuzz";
         squareElement.style.backgroundColor = "rgb(81, 10, 180)";
     }
