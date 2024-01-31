b122386b27f2d7a726e623a23e5f8c351611cc63
MiriamEvardi <miriamevardi97@gmail.com>
2024-01-31 15:21:19 +0100

    styled and exercise completed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-//Scrivi un programma che stampi in console i numeri da 1 a 100,
-//ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
-//Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-
-
 //memorizzo la costante della griglia
 const gridElement = document.querySelector("#grid");
 
@@ -14,26 +9,34 @@ for (let i = 1; i <= 100; i++) {
 
     //li stampo in pagina
     gridElement.append(squareElement);
+
     squareElement.className = "square";
     squareElement.innerHTML = i;
+    squareElement.style.color = "white";
+    squareElement.style.fontWeight = "bold";
+    squareElement.style.fontSize = "24px";
+    squareElement.style.backgroundColor = "rgb(223, 194, 250";
 
     // (IF) voglio che i multipli di 3 abbiano stampato "Fizz" al posto del numero
     if (i % 3 == 0) {
         squareElement.className += " fizz";
-
+        squareElement.innerText = "Fizz";
+        squareElement.style.backgroundColor = "rgb(182, 100, 241)";
     }
 
     // (IF) voglio che i multipli di 5 abbiano stampato "Buzz"
     if (i % 5 == 0) {
         squareElement.className += " buzz";
-
+        squareElement.innerText = "Buzz";
+        squareElement.style.backgroundColor = "rgb(81, 157, 255)";
     }
 
     // (IF) voglio che sia i multipli di 3 che di 5 abbiano stampato "FizzBuzz"
     // altrimenti fare la moltiplicazione 3 * 5 ?
     if (i % 3 == 0 && i % 5 == 0) {
         squareElement.className = "square fizz buzz";
-
+        squareElement.innerText = "FizzBuzz";
+        squareElement.style.backgroundColor = "rgb(81, 10, 180)";
     }
 }
 
