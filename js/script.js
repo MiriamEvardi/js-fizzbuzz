fd673d59584e1cfd5bb802a4e48dc7a752ce05a3
MiriamEvardi <miriamevardi97@gmail.com>
2024-01-31 14:00:30 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Scrivi un programma che stampi in console i numeri da 1 a 100,
+//ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+//Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+
+//memorizzo la costante della griglia
+//creo il codice da ripetere da 1 a 100 e li stampo in pagina
+// (IF) voglio che i multipli di 3 abbiano stampato "Fizz" al posto del numero
+// (ELSE) voglio che i multipli di 5 abbiano stampato "Buzz"
+// (ELSE IF) voglio che sia i multipli di 3 che di 5 abbiano stampato "FizzBuzz"
+// (invertire ELSE con ELSE IF?)
